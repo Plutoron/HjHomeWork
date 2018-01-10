073b73f15c0330bec1790682e77309b17c7bef49
KY-PC2001A\suyl <suyunlongsy@gmail.com>
2018-01-10 17:20:43 +0800

    z1

--- Q3/Q3.js
+++ Q3/Q3.js
@@ -1,3 +1,6 @@
+
+// 简单的
+
 function hoverTransform(config){
 	var target = document.querySelector(config.target);
 	var formerStyle = formatStyle(config.formerStyle);
@@ -25,3 +28,50 @@ hoverTransform({
 	formerStyle: ['transform: rotate(90deg);'],
 	changedStyle: ['transform: rotate(0deg);','transition:all 1s;']	
 })
+
+
+// 面向对象???
+
+var hoverTransform = (function(){
+	
+	var hoverTransformFun = function(config){
+
+	}
+
+	hoverTransformFun.prototype.init = function(config) {
+
+		this.target = document.querySelector(config.target);
+		this.formerStyle = formatStyle(config.formerStyle);
+		this.changedStyle = formatStyle(config.changedStyle)
+
+		return this;
+	};
+
+	hoverTransformFun.prototype.render = function() {
+		var that = this;
+		this.target.setAttribute("style",that.formerStyle);
+   		this.target.onmouseover = function(){ this.setAttribute("style",that.changedStyle) }
+
+	};
+	//返回构造函数
+	return hoverTransformFun;
+
+	// 工具函数
+	var formatStyle = function(styleArr){
+		var str = '';
+		styleArr.forEach((v,i,a)=>{
+			str += v
+		})
+		return str;
+	}
+
+})();
+
+new hoverTransform().init({
+	target: '.course_logo img',
+	formerStyle: ['transform: rotate(90deg);'],
+	changedStyle: ['transform: rotate(0deg);','transition:all 1s;']	
+}).render()
+
+
+
